cb7146e2ef60a58466654e18a11262c21fa9be0a
Crowdbotics <team@crowdbotics.com>
2022-01-26 19:07:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mydeliveryapp2_33190/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mydeliveryapp2_33190;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mydeliveryapp2_33190";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mydeliveryapp2_33190",
+  url: "https://mydeliveryapp2_33190.botics.co",
+  api: "https://mydeliveryapp2_33190.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
